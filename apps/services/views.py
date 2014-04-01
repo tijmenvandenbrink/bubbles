baac25fc34b3de808d99574a799d4f51d85d70dc
Tijmen van den Brink <tijmen.vandenbrink@gmail.com>
2014-04-01 15:05:30 +0200

    Added pagination to services.html

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import PageNotAnInteger, EmptyPage
 
 from apps.services.models import Service
 from apps.statistics.models import DataSource
 from apps.core.utils import create_multibarchart
+from apps.core.diggpaginator import DiggPaginator
 
 
 def services_list(request):
-    service_list = Service.objects.all().prefetch_related('organization',
-                                                          'status',
-                                                          'service_type').order_by('name')
+    services = Service.objects.all().prefetch_related('organization', 'status', 'service_type').order_by('name')
+
+    paginator = DiggPaginator(services, 25, body=10, padding=2, margin=2, )
+    page = request.GET.get('page')
+
+    try:
+        service_list = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        service_list = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        service_list = paginator.page(paginator.num_pages)
+
     return render(request, 'services.html', {"service_list": service_list})
 
 
